aef5009b71227921abce08ce7467a92b3db5396a
OkcuYazilimci <umutuygun.u@gmail.com>
2024-09-11 22:18:29 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java_leetcode.FirstOccurrence.FirstOccurrence;
 import java_leetcode.find_the_difference.FindTheDifference;
 import java_leetcode.find_the_difference.FindTheDifference2;
 
 public class Main {
     public static void main(String[] args) {
+        FirstOccurrence firstOccurrence = new FirstOccurrence();
         FindTheDifference solution = new FindTheDifference();
         FindTheDifference2 solution2 = new FindTheDifference2();
 
@@ -10,6 +12,7 @@ public class Main {
         System.out.println(solution.findTheDifference("umutuygun", "tumunugyuy"));
         System.out.println("-----------------------");
         System.out.println(solution2.findTheDifference2("umutuygun", "tumunugyuy"));
-        System.out.println("--------------------------------FindTheDifference--------------------------------");
+        System.out.println("----------------------------------------------------------------");
+        System.out.println(firstOccurrence.strStr("mississippi", "issip"));
     }
 }
---
+++ src/java_leetcode/FirstOccurrence/FirstOccurrence.java
@@ -0,0 +1,15 @@
+package java_leetcode.FirstOccurrence;
+
+public class FirstOccurrence {
+    public int strStr(String haystack, String needle) {
+        if (haystack.length() < needle.length()) return -1;
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++){
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                System.out.println(haystack.substring(i, i + needle.length()));
+                return i;
+            }
+        }
+        return -1;
+    }
+}
